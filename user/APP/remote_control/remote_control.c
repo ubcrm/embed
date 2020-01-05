9fa0f1ddcc74b5a0621f2df9bd2f351187ee804b
jirm2019 <jachnoin2@gmail.com>
2020-01-05 11:31:46 -0800

    added dji rc code

--- user/APP/remote_control/remote_control.c
+++ user/APP/remote_control/remote_control.c
@@ -27,31 +27,31 @@
 //遥控器出错数据上限
 #define RC_CHANNAL_ERROR_VALUE 700
 
-//取正函数
+// Function prototypes
 static int16_t RC_abs(int16_t value);
-//遥控器处理函数
 static void SBUS_TO_RC(volatile const uint8_t *sbus_buf, RC_ctrl_t *rc_ctrl);
 
-//遥控器控制变量
+// Variable definitions
+
+// A struct that holds all of the rc data
 static RC_ctrl_t rc_ctrl;
-//接收原始数据，为18个字节，给了36个字节长度，防止DMA传输越界
+// A buffer for holding the rc data, it is 2d to avoid issues with DMA
 static uint8_t SBUS_rx_buf[2][SBUS_RX_BUF_NUM];
 
-//初始化DMA，串口1
+// Configure USART and DMA pins, etc. for the rc controller
 void remote_control_init(void)
 {
     RC_Init(SBUS_rx_buf[0], SBUS_rx_buf[1], SBUS_RX_BUF_NUM);
 }
-//返回遥控器控制变量，通过指针传递方式传递信息
+// get a pointer to the rc struct
 const RC_ctrl_t *get_remote_control_point(void)
 {
     return &rc_ctrl;
 }
 
-//判断遥控器数据是否出错，
+// Determine if there is an error in the rc data
 uint8_t RC_data_is_error(void)
 {
-    //使用了go to语句 方便出错统一处理遥控器变量数据归零
     if (RC_abs(rc_ctrl.rc.ch[0]) > RC_CHANNAL_ERROR_VALUE)
     {
         goto error;
@@ -104,7 +104,8 @@ void slove_data_error(void)
     RC_restart(SBUS_RX_BUF_NUM);
 }
 
-//串口中断
+// USART1 interrupt handler. Transfers received data into the
+// buffer, and then struct when all data is received.
 void USART1_IRQHandler(void)
 {
     if (USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)
@@ -157,7 +158,7 @@ void USART1_IRQHandler(void)
     }
 }
 
-//取正函数
+// Absolute value
 static int16_t RC_abs(int16_t value)
 {
     if (value > 0)
@@ -169,6 +170,8 @@ static int16_t RC_abs(int16_t value)
         return -value;
     }
 }
+
+// Transfer rc data from the buffer to the struct.
 static void SBUS_TO_RC(volatile const uint8_t *sbus_buf, RC_ctrl_t *rc_ctrl)
 {
     if (sbus_buf == NULL || rc_ctrl == NULL)
@@ -179,11 +182,10 @@ static void SBUS_TO_RC(volatile const uint8_t *sbus_buf, RC_ctrl_t *rc_ctrl)
     rc_ctrl->rc.ch[0] = (sbus_buf[0] | (sbus_buf[1] << 8)) & 0x07ff;        //!< Channel 0
     rc_ctrl->rc.ch[1] = ((sbus_buf[1] >> 3) | (sbus_buf[2] << 5)) & 0x07ff; //!< Channel 1
     rc_ctrl->rc.ch[2] = ((sbus_buf[2] >> 6) | (sbus_buf[3] << 2) |          //!< Channel 2
-                         (sbus_buf[4] << 10)) &
-                        0x07ff;
+                         (sbus_buf[4] << 10)) & 0x07ff;
     rc_ctrl->rc.ch[3] = ((sbus_buf[4] >> 1) | (sbus_buf[5] << 7)) & 0x07ff; //!< Channel 3
-    rc_ctrl->rc.s[0] = ((sbus_buf[5] >> 4) & 0x0003);                  //!< Switch left
-    rc_ctrl->rc.s[1] = ((sbus_buf[5] >> 4) & 0x000C) >> 2;                       //!< Switch right
+    rc_ctrl->rc.s[0] = ((sbus_buf[5] >> 4) & 0x0003);                       //!< Switch left
+    rc_ctrl->rc.s[1] = ((sbus_buf[5] >> 4) & 0x000C) >> 2;                  //!< Switch right
     rc_ctrl->mouse.x = sbus_buf[6] | (sbus_buf[7] << 8);                    //!< Mouse X axis
     rc_ctrl->mouse.y = sbus_buf[8] | (sbus_buf[9] << 8);                    //!< Mouse Y axis
     rc_ctrl->mouse.z = sbus_buf[10] | (sbus_buf[11] << 8);                  //!< Mouse Z axis
