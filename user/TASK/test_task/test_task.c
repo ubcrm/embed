88c702b4a1dc46c472cdf0ab72b03721acbd7c40
JoshMarangoni <joshua.marangoni@mimik.com>
2020-01-19 12:09:17 -0800

    need to test pid

--- user/TASK/test_task/test_task.c
+++ user/TASK/test_task/test_task.c
@@ -11,29 +11,36 @@
 #include <stdio.h>
 
 // TODO: determine pid constants
-#define pid_kp 1
+#define pid_kp 2000
 #define pid_ki 0
 #define pid_kd 0
-#define max_out 0
+#define max_out 6000
 #define max_iout 0
+
+// #define CHASSIS_CONTROL_TIME_MS 1000
 
 Gimbal_Motor_t gimbal_yaw_motor;
 
 void testTask(void *pvParameters)
 {
-    gimbal_yaw_motor.gimbal_motor_raw = get_Yaw_Gimbal_Motor_Measure_Point();
+    gimbal_yaw_motor.gimbal_motor_raw = get_Pitch_Gimbal_Motor_Measure_Point(); // get_Yaw_Gimbal_Motor_Measure_Point();
 
-    int16_t yaw_current;
+    int16_t yaw_current;  // electric current?
 
     PidTypeDef pid;
     fp32 pid_constants[3] = {pid_kp, pid_ki, pid_kd};
     PID_Init(&pid, PID_POSITION, pid_constants, max_out, max_iout);
+        
+    CAN_CMD_GIMBAL(0, 0, 0, 0);
+    
+    int move = 0;
+    int throttle = 1;
     
     while(1) 
     {
         led_green_toggle();
 
-        //Get CAN received data
+        // Get CAN received data
         gimbal_yaw_motor.gimbal_pos_raw = gimbal_yaw_motor.gimbal_motor_raw->ecd;
         gimbal_yaw_motor.gimbal_speed_raw = gimbal_yaw_motor.gimbal_motor_raw->speed_rpm;
         gimbal_yaw_motor.gimbal_tq_current_raw = gimbal_yaw_motor.gimbal_motor_raw->given_current;
@@ -41,12 +48,29 @@ void testTask(void *pvParameters)
         yaw_current = PID_Calc(&pid, 0, gimbal_yaw_motor.gimbal_pos_raw); //TODO: confirm ref and set values
 
         //Make the motor turn
-        CAN_CMD_GIMBAL(yaw_current, 0, 0, 0);
+        CAN_CMD_GIMBAL(0, yaw_current, 0, 0);
 
+        /*
+        if (throttle)  
+        {
+            move += 4000;
+            CAN_CMD_GIMBAL(0, move, 0, 0);
+            move -= 4000;
+        }
+        else
+        {
+            move -= 4000;
+            CAN_CMD_GIMBAL(0, move, 0, 0);
+            move += 4000;
+        }
+        
+        throttle = !(throttle);
+        */
         //Sending data via UART
         send_to_uart(gimbal_yaw_motor, pid);			
 
-        delay_ms(100);
+        // delay_ms(10000);
+        // vTaskDelay(CHASSIS_CONTROL_TIME_MS);
     }
 }
 
