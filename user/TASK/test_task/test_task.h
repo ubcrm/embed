fab8f198fb5c7e2f6d4385a211d3db0577d3b1d3
Alice-Xiong <alicexxx0516@gmail.com>
2020-01-25 20:56:40 -0800

    cleaning up test task

--- user/TASK/test_task/test_task.h
+++ user/TASK/test_task/test_task.h
@@ -7,12 +7,21 @@
 
 typedef struct 
 {
-    const motor_measure_t *gimbal_motor_raw;
-    int16_t gimbal_pos_raw;
-    int16_t gimbal_speed_raw;
-    int16_t gimbal_tq_current_raw;
-	
+	const motor_measure_t *gimbal_motor_raw;
+	uint16_t pos_raw;
+	uint16_t speed_raw;
+	uint16_t current_raw;
 } Gimbal_Motor_t;
+
+
+typedef struct 
+{
+	Gimbal_Motor_t *yaw_motor;
+	Gimbal_Motor_t *pitch_motor;
+	const fp32 *gyro_reading_raw;
+	const fp32 *acce_reading_raw;
+    const fp32 *angle_reading_raw;
+} Gimbal_t;
 
 void testTask(void *pvParameters);
 void send_to_uart(Gimbal_Motor_t gimbal_yaw_motor, PidTypeDef pi, fp32 pitch_signal);
