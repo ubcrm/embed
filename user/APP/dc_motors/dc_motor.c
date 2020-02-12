87e3d2bb2e648e5ee4f4ade1569099f23c38c81c
kricha02 <t5z2b@ugrad.cs.ubc.ca>
2020-02-11 20:51:11 -0800

    Started on DC motors. TODO: Migrate DC motors to embedded-common or engineer robots.

---
+++ user/APP/dc_motors/dc_motor.c
@@ -0,0 +1,110 @@
+#include "dc_motor.h"
+#include "stm32f4xx.h"
+
+dc_motor dcm;
+
+void setup_pwm();
+void setup_gpio();
+
+void init_dc_motor(){
+    setup_pwm();
+    setup_gpio();
+}
+
+void dc_motor_set_vel(int vel){
+    TIM_SetCompare1(TIM1, vel); //TODO not TIM1 necessarly
+}
+
+int dc_motor_read_pos(){
+    return 0;
+}
+
+//Private functions for hardware
+void setup_pwm(){ //TODO change port
+    GPIO_InitTypeDef GPIO_InitStructure;
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStructure;
+    TIM_OCInitTypeDef TIM_OCInitStructure;
+
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+    //RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
+
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource8, GPIO_AF_TIM1);
+    //GPIO_PinAFConfig(GPIOE, GPIO_PinSource14, GPIO_AF_TIM1);
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    //GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14;
+    //GPIO_Init(GPIOE, &GPIO_InitStructure);
+
+    RCC_APB2PeriphResetCmd(RCC_APB2Periph_TIM1, ENABLE);
+    RCC_APB2PeriphResetCmd(RCC_APB2Periph_TIM1, DISABLE);
+
+    TIM_TimeBaseInitStructure.TIM_Period = 2000 - 1;
+    TIM_TimeBaseInitStructure.TIM_Prescaler = 180 - 1;
+    TIM_TimeBaseInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+
+    TIM_TimeBaseInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+
+    TIM_TimeBaseInit(TIM1, &TIM_TimeBaseInitStructure);
+
+    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+    TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+    TIM_OCInitStructure.TIM_OutputNState = TIM_OutputNState_Disable;
+    TIM_OCInitStructure.TIM_OCNPolarity = TIM_OCNPolarity_Low;
+    TIM_OCInitStructure.TIM_OCIdleState = TIM_OCIdleState_Set;
+    TIM_OCInitStructure.TIM_OCNIdleState = TIM_OCIdleState_Reset;
+    TIM_OCInitStructure.TIM_Pulse = 1000;
+
+    TIM_OC1Init(TIM1, &TIM_OCInitStructure);
+    //TIM_OC4Init(TIM1, &TIM_OCInitStructure);
+
+    TIM_OC1PreloadConfig(TIM1, TIM_OCPreload_Enable);
+    //TIM_OC4PreloadConfig(TIM1, TIM_OCPreload_Enable);
+
+    TIM_ARRPreloadConfig(TIM1, ENABLE);
+
+    TIM_CtrlPWMOutputs(TIM1, ENABLE);
+
+    TIM_Cmd(TIM1, ENABLE);
+
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOF, ENABLE);
+    /*GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+    GPIO_Init(GPIOF, &GPIO_InitStructure);
+    GPIO_SetBits(GPIOF, GPIO_Pin_10);*/
+}
+
+void setup_gpio(){ //TODO change port
+    GPIO_InitTypeDef GPIO_InitStructure;
+
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOF | RCC_AHB1Periph_GPIOE | RCC_AHB1Periph_GPIOG, ENABLE); //
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+    GPIO_Init(GPIOF, &GPIO_InitStructure);
+
+    //GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
+    //GPIO_Init(GPIOE, &GPIO_InitStructure);
+
+    //led_green_off();
+    //led_red_off();
+
+    //GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8;
+    //GPIO_Init(GPIOG, &GPIO_InitStructure);
+
+    //flow_led_off(GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8);
+}
